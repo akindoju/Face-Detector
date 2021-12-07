0c75006dce0690800186b881c720073500e5d0a6
akindoju <davidd.akindoju@gmail.com>
2021-12-07 18:11:10 +0100

    minor changes for local deployment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import React, {Component} from 'react';
-import Navigation from './Components/Navigation/Navigation';
-import Logo from './Components/Logo/Logo';
-import ImageLinkform from './Components/ILF/ImageLinkForm';
-import './App.css';
-import Rank from './Components/Rank/Rank'
-import Particles from 'react-particles-js';
-import FaceRecognition from './Components/FR/FaceRecognition';
-import SignIn from './Components/SignIn/SignIn';
-import Register from './Components/Register/Register';
-
-
+import React, { Component } from "react";
+import Navigation from "./Components/Navigation/Navigation";
+import Logo from "./Components/Logo/Logo";
+import ImageLinkform from "./Components/ILF/ImageLinkForm";
+import "./App.css";
+import Rank from "./Components/Rank/Rank";
+import Particles from "react-particles-js";
+import FaceRecognition from "./Components/FR/FaceRecognition";
+import SignIn from "./Components/SignIn/SignIn";
+import Register from "./Components/Register/Register";
 
 const particlesOption = {
   particles: {
@@ -17,127 +15,164 @@ const particlesOption = {
       value: 30,
       density: {
         enable: true,
-        value_area: 800
-      }
-    }
-  }
-}
+        value_area: 800,
+      },
+    },
+  },
+};
 
 const initialState = {
-  input: '',
-  imageUrl: '',
-  box:{ },
-  route: 'SignIn',
+  input: "",
+  imageUrl: "",
+  box: {},
+  route: "SignIn",
   isSignedIn: false,
   user: {
-    id: '',
-    name: '',
-    email: '',
+    id: "",
+    name: "",
+    email: "",
     entries: 0,
-    joined: ''
-  }
-}
+    joined: "",
+  },
+};
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super();
     this.state = initialState;
   }
 
-    loadUser = (data) => {
-      this.setState(
-        {user: {
-          id: data.id,
-          name: data.name,
-          email: data.email,
-          entries: data.entries,
-          joined: data.joined
-          }
-        }
-      )
-    }
-  
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
 
   faceCalculations = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputImage');
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
     const width = Number(image.width);
     const height = Number(image.height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
-  }
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
 
   faceBox = (box) => {
-    this.setState({box: box})
-  }
+    this.setState({ box: box });
+  };
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value})
-  }
+    this.setState({ input: event.target.value });
+  };
+
+  // onButtonSubmit = () => {
+  //   this.setState({imageUrl: this.state.input});
+  //   fetch('https://calm-tundra-76384.herokuapp.com/imageUrl', {
+  //         method: 'post',
+  //         headers: {'Content-Type': 'application/json'},
+  //         body: JSON.stringify({
+  //             input: this.state.input
+  //         })
+  //       })
+  //       .then(response => response.json())
+  //       .then(response => {
+  //         if(response) {
+  //             fetch('https://calm-tundra-76384.herokuapp.com/image', {
+  //               method: 'put',
+  //               headers: {'Content-Type': 'application/json'},
+  //               body: JSON.stringify({
+  //                   id: this.state.user.id
+  //               })
+  //           }).then(response => response.json())
+  //             .then(count => {
+  //               this.setState(Object.assign(this.state.user, {entries: count}))
+  //             })
+  //         }
+  //     this.faceBox(this.faceCalculations(response))
+  //     })
+  //   .catch(err => console.log(err));
+  // }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-    fetch('https://calm-tundra-76384.herokuapp.com/imageUrl', {
-          method: 'post',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-              input: this.state.input
+    this.setState({ imageUrl: this.state.input });
+    fetch("http://localhost:3000/imageUrl", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        input: this.state.input,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response) {
+          fetch("http://localhost:3000/image", {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
           })
-        })
-        .then(response => response.json())
-        .then(response => {
-          if(response) {
-              fetch('https://calm-tundra-76384.herokuapp.com/image', {
-                method: 'put',
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({
-                    id: this.state.user.id
-                })
-            }).then(response => response.json())
-              .then(count => {
-                this.setState(Object.assign(this.state.user, {entries: count}))
-              })
-          }
-      this.faceBox(this.faceCalculations(response))
+            .then((response) => response.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count }));
+            });
+        }
+        this.faceBox(this.faceCalculations(response));
       })
-    .catch(err => console.log(err));
-  }
+      .catch((err) => console.log(err));
+  };
 
   onRouteChange = (route) => {
-    if(route === 'SignIn') {
-      this.setState(initialState)
-    } else if (route === 'home') {
-      this.setState({isSignedIn: true})
+    if (route === "SignIn") {
+      this.setState(initialState);
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
     }
-    this.setState({route: route});
-  }
+    this.setState({ route: route });
+  };
 
   render() {
-    const { isSignedIn, box, imageUrl, route} = this.state
+    const { isSignedIn, box, imageUrl, route } = this.state;
     return (
       <div className="App">
-        <Particles className = 'particles'
-          params = {{particlesOption}}
+        <Particles className="particles" params={{ particlesOption }} />
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
         />
-        <Navigation isSignedIn = {isSignedIn} onRouteChange = {this.onRouteChange} />
-        { route === 'home'
-          ? <div>
-            <Logo/>
-            <Rank name={this.state.user.name} entries={this.state.user.entries}/>
-            <ImageLinkform onInputChange = {this.onInputChange} onButtonSubmit = {this.onButtonSubmit}/>
-            <FaceRecognition box = {box} imageUrl = {imageUrl}/>
+        {route === "home" ? (
+          <div>
+            <Logo />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
+            <ImageLinkform
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
           </div>
-          :(
-            route === 'SignIn'
-            ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
-            : <Register loadUser = {this.loadUser} onRouteChange = {this.onRouteChange}/>
-          )
-        }
-        </div>
+        ) : route === "SignIn" ? (
+          <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        )}
+      </div>
     );
   }
 }
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,106 +1,131 @@
-import React from 'react'
-
+import React from "react";
 
 class Register extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            Password: '',
-            Email: '',
-            Name: ''
+  constructor(props) {
+    super(props);
+    this.state = {
+      Password: "",
+      Email: "",
+      Name: "",
+    };
+  }
+
+  onEmailChange = (event) => {
+    this.setState({ Email: event.target.value });
+  };
+
+  onPasswordChange = (event) => {
+    this.setState({ Password: event.target.value });
+  };
+
+  onNameChange = (event) => {
+    this.setState({ Name: event.target.value });
+  };
+
+  // onSubmitSignIn = () => {
+  //         fetch('https://calm-tundra-76384.herokuapp.com/Register', {
+  //             method: 'post',
+  //             headers: {'Content-Type': 'application/json'},
+  //             body: JSON.stringify({
+  //                 email: this.state.Email,
+  //                 password: this.state.Password,
+  //                 name: this.state.Name
+  //             })
+  //         })
+  //         .then(response => response.json())
+  //             .then(user => {
+  //                 if(user.id) {
+  //                     this.props.loadUser(user)
+  //                     this.props.onRouteChange('home')
+  //                 }
+  //             })
+  //     }
+
+  onSubmitSignIn = () => {
+    fetch("http://localhost:3000/Register", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: this.state.Email,
+        password: this.state.Password,
+        name: this.state.Name,
+      }),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.id) {
+          this.props.loadUser(user);
+          this.props.onRouteChange("home");
         }
-    }
+      });
+  };
 
-    onEmailChange = (event) => {
-        this.setState({Email: event.target.value})
-    }
-
-    onPasswordChange = (event) => {
-        this.setState({Password: event.target.value})
-    }
-
-    onNameChange = (event) => {
-        this.setState({Name: event.target.value})
-    }
-
-onSubmitSignIn = () => {
-        fetch('https://calm-tundra-76384.herokuapp.com/Register', {
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                email: this.state.Email,
-                password: this.state.Password,
-                name: this.state.Name
-            })
-        })
-        .then(response => response.json())
-            .then(user => {
-                if(user.id) {
-                    this.props.loadUser(user)
-                    this.props.onRouteChange('home')
-                }
-            })
-    }
-
-    render () {
-        return (
-            <article className="br3 ba shadow-5 b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
-            <main className="pa4 black-80">
-                <div className="measure">
-                    <fieldset id="sign_up" className="ba b--transparent ph0 mt4">
-                        <legend className="f2 fw6 ph0 mh0">Register</legend>
-                        <div className="mt3">
-                        <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-                        <input 
-                            className="b--black f6 w-90 ba br2 center pa2 input-reset bg-transparent hover-bg-black hover-white " 
-                            type="name" 
-                            name="name"  
-                            id="name"
-                            onChange = {this.onNameChange}
-                        />
-                        </div>
-                        <div className="mt3">
-                        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                        <input 
-                            className="b--black f6 w-100 ba br2 center pa2 input-reset bg-transparent hover-bg-black hover-white " 
-                            type="email" 
-                            name="email-address"  
-                            id="email-address"
-                            onChange = {this.onEmailChange}
-                        />
-                        </div>
-                        <div className="mv3">
-                        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                        <input 
-                            className="b--black f6 w-100 ba br2 center pa2 input-reset bg-transparent hover-bg-black hover-white " 
-                            type="password" 
-                            name="password"  
-                            id="password"
-                            onChange = {this.onPasswordChange}
-                        />
-                        </div>
-                    </fieldset>
-                    <div className="">
-                        <input
-                            onClick = {this.onSubmitSignIn}
-                            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                            type="submit" 
-                            value="Register"/>
-                    </div>
-                    <div className="lh-copy mt3">
-                        <p href="#0" className="f6 link dim black db pointer" onClick = {() => this.props.onRouteChange('SignIn')}>Sign In?</p>
-                    </div>
-                </div>
-            </main>
-            </article> 
-        );
-    }
+  render() {
+    return (
+      <article className="br3 ba shadow-5 b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
+        <main className="pa4 black-80">
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mt4">
+              <legend className="f2 fw6 ph0 mh0">Register</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="name">
+                  Name
+                </label>
+                <input
+                  className="b--black f6 w-90 ba br2 center pa2 input-reset bg-transparent hover-bg-black hover-white "
+                  type="name"
+                  name="name"
+                  id="name"
+                  onChange={this.onNameChange}
+                />
+              </div>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
+                <input
+                  className="b--black f6 w-100 ba br2 center pa2 input-reset bg-transparent hover-bg-black hover-white "
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  onChange={this.onEmailChange}
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="b--black f6 w-100 ba br2 center pa2 input-reset bg-transparent hover-bg-black hover-white "
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={this.onPasswordChange}
+                />
+              </div>
+            </fieldset>
+            <div className="">
+              <input
+                onClick={this.onSubmitSignIn}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Register"
+              />
+            </div>
+            <div className="lh-copy mt3">
+              <p
+                href="#0"
+                className="f6 link dim black db pointer"
+                onClick={() => this.props.onRouteChange("SignIn")}
+              >
+                Sign In?
+              </p>
+            </div>
+          </div>
+        </main>
+      </article>
+    );
+  }
 }
 
-
-
 export default Register;
-
-
-
-
