b0c1edf655b96eb7e165fdc045a47c74d462cf93
akindoju <davidd.akindoju@gmail.com>
2021-12-24 14:15:26 +0100

    Created dropdown menu from avatar

--- src/Components/Profile/ProfileIcon.js
+++ src/Components/Profile/ProfileIcon.js
@@ -1,21 +1,51 @@
 import React from "react";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 
 class ProfileIcon extends React.Component {
   constructor(props) {
-    super();
+    super(props);
     this.state = {
       dropdownOpen: false,
     };
   }
 
+  toggle = () => {
+    this.setState((prevState) => ({
+      dropdownOpen: !prevState.dropdownOpen,
+    }));
+  };
+
   render() {
     return (
       <div className="pa4 tc">
-        <img
-          src="http://tachyons.io/img/logo.jpg"
-          className="br-100 ba h3 w3 dib"
-          alt="avatar"
-        />
+        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+          <DropdownToggle
+            tag="span"
+            data-toggle="dropdown"
+            aria-expanded={this.state.dropdownOpen}
+          >
+            <img
+              src="http://tachyons.io/img/logo.jpg"
+              className="br-100 ba h3 w3 dib"
+              alt="avatar"
+            />
+          </DropdownToggle>
+          <DropdownMenu
+            className="b--transparent shadow-5"
+            style={{
+              marginTop: "20px",
+              backgroundColor: "rgba(255,255,255,0.5)",
+            }}
+          >
+            <DropdownItem>View Profile</DropdownItem>
+            <DropdownItem>Sign out</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
       </div>
     );
   }
