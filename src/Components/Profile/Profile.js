752c6b11699dbaa78474a937127c017a031829a5
akindoju <davidd.akindoju@gmail.com>
2021-12-25 14:28:04 +0100

    Added live update to name

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,75 +1,107 @@
 import React from "react";
 import "./Profile.css";
 
-const Profile = ({ isProfileOpen, toggleModal, user }) => {
-  return (
-    <div className="profile-modal">
-      <article className="br3 ba shadow-5 b--black-10 mv4 w-100 w-50-m w-25-l mw6 center bg-white">
-        <main className="pa4 black-80 w-80">
-          <img
-            src="http://tachyons.io/img/logo.jpg"
-            className="h3 w3 dib"
-            alt="avatar"
-          />
-          <h1>{user.name}</h1>
-          <h4>{`Images Submitted: ${user.entries}`}</h4>
-          <p>{`Member since: ${new Date(user.joined).toLocaleDateString()}`}</p>
-          <hr />
-          <label className="mt2 fw6" htmlFor="username">
-            Name:
-          </label>
-          <input
-            className="pa2 ba w-100"
-            placeholder={user.name}
-            type="text"
-            name="username"
-            id="name"
-          />
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: this.props.user.name,
+      age: this.props.user.age,
+      pet: this.props.user.pet,
+    };
+  }
 
-          <label className="mt2 fw6" htmlFor="age">
-            Age:
-          </label>
-          <input
-            className="pa2 ba w-100"
-            placeholder={user.age}
-            type="text"
-            name="age"
-            id="age"
-          />
+  onFormChange = ({ target }) => {
+    switch (target.name) {
+      case "user-name":
+        this.setState({ name: target.value });
+        break;
+      case "user-age":
+        this.setState({ age: target.value });
+        break;
+      case "user-pet":
+        this.setState({ pet: target.value });
+        break;
+      default:
+        return;
+    }
+  };
 
-          <label className="mt2 fw6" htmlFor="pet">
-            Pet:
-          </label>
-          <input
-            className="pa2 ba w-100"
-            placeholder={user.pet}
-            type="text"
-            name="pet"
-            id="pet"
-          />
+  render() {
+    return (
+      <div className="profile-modal">
+        <article className="br3 ba shadow-5 b--black-10 mv4 w-100 w-50-m w-25-l mw6 center bg-white">
+          <main className="pa4 black-80 w-80">
+            <img
+              src="http://tachyons.io/img/logo.jpg"
+              className="h3 w3 dib"
+              alt="avatar"
+            />
+            <h1>{this.stae.name}</h1>
+            <h4>{`Images Submitted: ${this.props.user.entries}`}</h4>
+            <p>{`Member since: ${new Date(
+              this.props.user.joined
+            ).toLocaleDateString()}`}</p>
+            <hr />
+            <label className="mt2 fw6" htmlFor="user-name">
+              Name:
+            </label>
+            <input
+              onChange={() => this.onFormChange()}
+              className="pa2 ba w-100"
+              placeholder={this.props.user.name}
+              type="text"
+              name="username"
+              id="name"
+            />
 
-          <div
-            className="mt4"
-            style={{ display: "flex", justifyContent: "space-evenly" }}
-          >
-            <button className="b pa2 grow pointer hover-white w-40 bg-light-blue b--black-20">
-              Save
-            </button>
+            <label className="mt2 fw6" htmlFor="user-age">
+              Age:
+            </label>
+            <input
+              onChange={() => this.onFormChange()}
+              className="pa2 ba w-100"
+              placeholder={this.props.user.age}
+              type="text"
+              name="age"
+              id="age"
+            />
 
-            <button
-              className="b pa2 grow pointer hover-white w-40 bg-light-red b--black-20"
-              onClick={() => toggleModal()}
+            <label className="mt2 fw6" htmlFor="user-pet">
+              Pet:
+            </label>
+            <input
+              onChange={() => this.onFormChange()}
+              className="pa2 ba w-100"
+              placeholder={this.props.user.pet}
+              type="text"
+              name="pet"
+              id="pet"
+            />
+
+            <div
+              className="mt4"
+              style={{ display: "flex", justifyContent: "space-evenly" }}
             >
-              Cancel
-            </button>
+              <button className="b pa2 grow pointer hover-white w-40 bg-light-blue b--black-20">
+                Save
+              </button>
+
+              <button
+                className="b pa2 grow pointer hover-white w-40 bg-light-red b--black-20"
+                onClick={() => this.props.toggleModal()}
+              >
+                Cancel
+              </button>
+            </div>
+          </main>
+          <div className="modal-close" onClick={() => this.props.toggleModal()}>
+            &times;
           </div>
-        </main>
-        <div className="modal-close" onClick={() => toggleModal()}>
-          &times;
-        </div>
-      </article>
-    </div>
-  );
-};
+        </article>
+      </div>
+    );
+  }
+}
 
 export default Profile;
