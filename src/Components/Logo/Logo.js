d0dde212d9d26ad35f335151895d6e2c1dd6e05e
akindoju <davidd.akindoju@gmail.com>
2022-01-01 15:42:32 +0100

    Changed favicon and logo

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,16 +1,28 @@
-import React from 'react'
-import Tilt from 'react-tilt'
-import './Logo.css'
-import logo from './logo.png'
+import React from "react";
+import Tilt from "react-tilt";
+import "./Logo.css";
+import logo from "./logo.png";
 
 const Logo = () => {
-    return(
-        <div className = 'ma4 mt0 top'>
-            <Tilt className = 'Tilt' options = {{max:25}} style = {{height: 150, width: 150}}>
-                <div className = 'Tilt-inner'> <img style = {{paddingTop: '30px'}} alt = 'logo' src = {logo} width = '100' height = '100'  /> </div>
-            </Tilt>
+  return (
+    <div className="ma4 mt0 top">
+      <Tilt
+        className="Tilt"
+        options={{ max: 25 }}
+        style={{ height: 150, width: 150 }}
+      >
+        <div className="Tilt-inner">
+          <img
+            style={{ paddingTop: "30px" }}
+            alt="logo"
+            src={logo}
+            width="100"
+            height="120"
+          />{" "}
         </div>
-    );
-}
+      </Tilt>
+    </div>
+  );
+};
 
-export default Logo
+export default Logo;
